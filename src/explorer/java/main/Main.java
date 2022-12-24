55b817608de1000bf3597962f420bcd2732e666c
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-24 14:46:30 +0600

    test

---
+++ src/explorer/java/main/Main.java
@@ -0,0 +1,16 @@
+package explorer.java.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+
+create new java application
+project name java shop app
+main class duke choice,shopapp
+print "Welcome to Duke Choice Shop"! from main methods
+
+ */
---
+++ src/explorer/java/main/ShopApp/ShopApp.java
@@ -0,0 +1,16 @@
+package explorer.java.main.ShopApp;
+
+public class ShopApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Jusan Mart!");
+    }
+
+}
+/*
+
+    1) create new java application
+    2) project name java shop app and package explorer.java.main
+    3) public class ShopApp
+    4) print "Welcome to Duke Choice Shop"! from main methods
+
+ */
